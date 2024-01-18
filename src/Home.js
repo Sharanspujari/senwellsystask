c3a2ad9af50937aa1b1bcf13d61302ca299be248
Sharanspujari <sharanspujari142@gmail.com>
2024-01-18 20:04:17 +0530

    final commit

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
 import useFetch from "./useFetch";
 
 const Home = () => {
   const { data, getApiData } = useFetch();
-  console.log("data:",data);
 
+  useEffect(() => {
+    console.log("data:", data);
+  }, [data]);
   return (
     <>
       <h2>NavBar</h2>
+      <button onClick={getApiData}>Refresh to get data</button>
     </>
   );
 };
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,17 +4,18 @@ import axios from "axios";
 const useFetch = () => {
   const [data, setData] = useState([]);
 
+  const getApiData = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/products/1");
+      setData(response.data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
   useEffect(() => {
-    const getApiData = async () => {
-      try {
-        const response = await axios.get("https://dummyjson.com/products/1");
-        setData(response.data);
-      } catch (error) {
-        console.log("Error:", error);
-      }
-    };
     getApiData();
   }, ["https://dummyjson.com/products/1"]);
-  return { data };
+  return { data, getApiData };
 };
 export default useFetch;
